cda7685a79641d64f305bf69b12fe02a21ad53f3
vaylor27 <godisagodlol@gmail.com>
2023-02-17 20:25:29 -0500

    Fixed Item Models

---
+++ src/main/java/net/vakror/soulbound/model/BakedItemModel.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+package net.vakror.soulbound.model;
+
+import javax.annotation.Nullable;
+
+import java.util.List;
+import java.util.Random;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.block.model.ItemTransform;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.state.BlockState;
+import com.mojang.math.Transformation;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
+import net.minecraftforge.client.model.BakedModelWrapper;
+
+public class BakedItemModel implements BakedModel
+{
+    protected final ImmutableList<BakedQuad> quads;
+    protected final TextureAtlasSprite particle;
+    protected final ImmutableMap<TransformType, ItemTransform> transforms;
+    protected final ItemOverrides overrides;
+    protected final BakedModel guiModel;
+    protected final boolean isSideLit;
+
+    public BakedItemModel(ImmutableList<BakedQuad> quads, TextureAtlasSprite particle, ImmutableMap<TransformType, ItemTransform> transforms, ItemOverrides overrides, boolean untransformed, boolean isSideLit)
+    {
+        this.quads = quads;
+        this.particle = particle;
+        this.transforms = transforms;
+        this.overrides = overrides;
+        this.isSideLit = isSideLit;
+        this.guiModel = untransformed && hasGuiIdentity(transforms) ? new BakedGuiItemModel<>(this) : null;
+    }
+
+    private static boolean hasGuiIdentity(ImmutableMap<TransformType, ItemTransform> transforms)
+    {
+        ItemTransform guiTransform = transforms.get(TransformType.GUI);
+        return guiTransform == null || guiTransform.equals(ItemTransform.NO_TRANSFORM);
+    }
+
+    @Override public boolean useAmbientOcclusion() { return true; }
+    @Override public boolean isGui3d() { return false; }
+    @Override public boolean usesBlockLight() { return isSideLit; }
+    @Override public boolean isCustomRenderer() { return false; }
+    @Override public TextureAtlasSprite getParticleIcon() { return particle; }
+    @Override public ItemOverrides getOverrides() { return overrides; }
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource rand)
+    {
+        if (side == null)
+        {
+            return quads;
+        }
+        return ImmutableList.of();
+    }
+
+    @Override
+    public BakedModel applyTransform(TransformType type, PoseStack poseStack, boolean applyLeftHandTransform) {
+        if (type == TransformType.GUI && this.guiModel != null)
+    {
+        return this.guiModel.applyTransform(type, poseStack, applyLeftHandTransform);
+    }
+        return handlePerspective(this, transforms, type, poseStack, applyLeftHandTransform);
+    }
+
+    @SuppressWarnings("all")
+    public static BakedModel handlePerspective(BakedModel model, ImmutableMap<ItemTransforms.TransformType, ItemTransform> transforms, ItemTransforms.TransformType cameraTransformType, PoseStack mat, boolean leftHand)
+    {
+        ItemTransform tr = transforms.getOrDefault(cameraTransformType, ItemTransform.NO_TRANSFORM);
+        if (!tr.equals(tr.NO_TRANSFORM)) {
+            tr.apply(leftHand, mat);
+        }
+        return model;
+    }
+
+    public static class BakedGuiItemModel<T extends BakedItemModel> extends BakedModelWrapper<T>
+    {
+        private final ImmutableList<BakedQuad> quads;
+
+        public BakedGuiItemModel(T originalModel)
+        {
+            super(originalModel);
+            ImmutableList.Builder<BakedQuad> builder = ImmutableList.builder();
+            for (BakedQuad quad : originalModel.quads)
+            {
+                if (quad.getDirection() == Direction.SOUTH)
+                {
+                    builder.add(quad);
+                }
+            }
+            this.quads = builder.build();
+        }
+
+        @Override
+        public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource rand)
+        {
+            if(side == null)
+            {
+                return quads;
+            }
+            return ImmutableList.of();
+        }
+
+        @Override
+        public BakedModel applyTransform(TransformType type, PoseStack poseStack, boolean doLeftHandTransformation)
+        {
+            if (type == TransformType.GUI)
+            {
+                return handlePerspective(this, originalModel.transforms, type, poseStack, doLeftHandTransformation);
+            }
+            return handlePerspective(this.originalModel, this.originalModel.transforms, type, poseStack, doLeftHandTransformation);
+        }
+    }
+}
