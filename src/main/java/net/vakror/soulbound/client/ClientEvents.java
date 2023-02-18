73c4fe753ce95b7f56c24714767f64113905724f
vaylor27 <godisagodlol@gmail.com>
2023-02-17 20:14:49 -0500

    Fix soul Overlay being on top of everything

--- src/main/java/net/vakror/soulbound/client/ClientEvents.java
+++ src/main/java/net/vakror/soulbound/client/ClientEvents.java
@@ -1,7 +1,8 @@
 package net.vakror.soulbound.client;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.gui.OverlayRegistry;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.gui.overlay.GuiOverlayManager;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -12,9 +13,8 @@ public class ClientEvents {
     @Mod.EventBusSubscriber(modid = SoulboundMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientForgeEvents {
         @SubscribeEvent
-        public static void registerGuiOverlay(FMLClientSetupEvent event) {
-            OverlayRegistry.registerOverlayTop("soul", SoulHudOverlay.HUD_SOUL);
-            OverlayRegistry.enableOverlay(SoulHudOverlay.HUD_SOUL, true);
+        public static void registerGuiOverlay(RegisterGuiOverlaysEvent event) {
+            event.registerBelowAll("soul", SoulHudOverlay.HUD_SOUL);
         }
     }
 }
