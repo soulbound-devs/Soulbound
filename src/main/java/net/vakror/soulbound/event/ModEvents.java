b46a02f315e640da00999ff4e2cffd5d862e267e
vaylor27 <godisagodlol@gmail.com>
2023-02-17 20:26:47 -0500

    Fix Errors caused by update

--- src/main/java/net/vakror/soulbound/event/ModEvents.java
+++ src/main/java/net/vakror/soulbound/event/ModEvents.java
@@ -1,30 +1,32 @@
 package net.vakror.soulbound.event;
 
-import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
+import net.minecraft.core.Vec3i;
 import net.minecraft.data.worldgen.placement.MiscOverworldPlacements;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 import net.vakror.soulbound.SoulboundMod;
 import net.vakror.soulbound.items.custom.WandItem;
-import net.vakror.soulbound.model.WandModel;
 import net.vakror.soulbound.networking.ModPackets;
 import net.vakror.soulbound.networking.SyncSoulS2CPacket;
 import net.vakror.soulbound.soul.PlayerSoul;
@@ -46,19 +48,43 @@ public class ModEvents {
                 }
             }
         }
+
+        @SubscribeEvent
+        public static void triggerScytheAOE(LivingHurtEvent event) {
+            Player player = Minecraft.getInstance().player;
+            if (player != null) {
+                if (player.getMainHandItem().getItem() instanceof WandItem || player.getOffhandItem().getItem() instanceof WandItem) {
+                    InteractionHand hand;
+                    if (player.getMainHandItem().getItem() instanceof WandItem) {
+                        hand = InteractionHand.MAIN_HAND;
+                    } else {
+                        hand = InteractionHand.OFF_HAND;
+                    }
+                    player.getItemInHand(hand).getCapability(ItemWandProvider.WAND).ifPresent(wand -> {
+                        List<Mob> nearbyMobs = getNearbyEntities(event.getEntity().level, player.blockPosition(), (float) 3, Mob.class);
+                        nearbyMobs.remove((Mob) event.getEntity());
+                        int i = 1;
+                        for (Mob mob : nearbyMobs) {
+                            mob.hurt(event.getSource(), event.getAmount());
+                            i++;
+                            if (i >= 50) {
+                                break;
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        public static <T extends Entity> List<T> getNearbyEntities(final LevelAccessor world, final Vec3i pos, final float radius, final Class<T> entityClass) {
+            final AABB selectBox = new AABB(0.0, 0.0, 0.0, 1.0, 1.0, 1.0).move((double) pos.getX(), (double) pos.getY(), (double) pos.getZ()).inflate((double) radius);
+            return (List<T>) world.getEntitiesOfClass(entityClass, selectBox, entity -> entity.isAlive() && !entity.isSpectator());
+        }
+
         @SubscribeEvent
         public static void onAttachCapabilitiesItem(AttachCapabilitiesEvent<ItemStack> event) {
             if (event.getObject().getItem() instanceof WandItem) {
                 event.addCapability(new ResourceLocation(SoulboundMod.MOD_ID, "seals"), new ItemWandProvider());
-            }
-        }
-
-        @SubscribeEvent
-        public static void onLoadBiomes(BiomeLoadingEvent event) {
-            if (event.getName().equals(new ResourceLocation(SoulboundMod.MOD_ID, "dead_forest"))) {
-                List<Holder<PlacedFeature>> base = event.getGeneration().getFeatures(GenerationStep.Decoration.VEGETAL_DECORATION);
-                base.add(MiscOverworldPlacements.FOREST_ROCK);
-                base.add(ModPlacedFeatures.ANCIENT_OAK_PLACED);
             }
         }
 
@@ -93,8 +119,8 @@ public class ModEvents {
         }
 
         @SubscribeEvent
-        public static void onPlayerJoinWorld(EntityJoinWorldEvent event) {
-            if (!event.getWorld().isClientSide) {
+        public static void onPlayerJoinWorld(EntityJoinLevelEvent event) {
+            if (!event.getLevel().isClientSide) {
                 if (event.getEntity() instanceof ServerPlayer player) {
                     player.getCapability(PlayerSoulProvider.PLAYER_SOUL).ifPresent(soul -> {
                         ModPackets.sendToClient(new SyncSoulS2CPacket(soul.getSoul(), soul.getMaxSoul(), soul.getDarkSoul(), soul.getMaxDarkSoul()), player);
