4565e3be03cf4911f13b09144f630e150036ac54
vaylor27 <godisagodlol@gmail.com>
2023-02-17 20:24:37 -0500

    Add corrupted tree

--- src/main/java/net/vakror/soulbound/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/vakror/soulbound/world/feature/ModConfiguredFeatures.java
@@ -4,25 +4,22 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.levelgen.feature.BlockBlobFeature;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.BlockStateConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.DarkOakTrunkPlacer;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.ForkingTrunkPlacer;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.vakror.soulbound.SoulboundMod;
 import net.vakror.soulbound.blocks.ModBlocks;
@@ -31,20 +28,31 @@ import java.util.List;
 
 public class ModConfiguredFeatures {
 
+    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
+            DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, SoulboundMod.MOD_ID);
 
-    public static final Holder<ConfiguredFeature<TreeConfiguration, ?>> ANCIENT_OAK_TREE =
-            FeatureUtils.register("ancient_oak", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
-                    BlockStateProvider.simple(ModBlocks.ANCIENT_OAK_LOG.get()),
-                    new DarkOakTrunkPlacer(6, 3, 4),
-                    BlockStateProvider.simple(Blocks.AIR),
-                    new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
-                    new TwoLayersFeatureSize(1, 0, 2)).build());
+    public static final RegistryObject<ConfiguredFeature<?, ?>> CORRUPTED_TREE =
+            CONFIGURED_FEATURES.register("corrupted_tree", () ->
+                    new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
+                            BlockStateProvider.simple(ModBlocks.CORRUPTED_LOG.get()),
+                            new StraightTrunkPlacer(1, 1, 1),
+                            BlockStateProvider.simple(ModBlocks.CORRUPTED_LEAVES.get()),
+                            new BlobFoliagePlacer(ConstantInt.of(1), ConstantInt.of(2), 1),
+                            new TwoLayersFeatureSize(1, 1, 2)).dirt(BlockStateProvider.simple(Blocks.SCULK)).build()));
 
-    public static final Holder<PlacedFeature> ANCIENT_OAK_CHECKED =
-            PlacementUtils.register("ancient_oak_checked", ANCIENT_OAK_TREE, PlacementUtils.filteredByBlockSurvival(Blocks.GRASS_BLOCK));
 
-    public static final Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> ANCIENT_OAK_SPAWN =
-            FeatureUtils.register("ancient_oak_spawn", Feature.RANDOM_SELECTOR,
-                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(ANCIENT_OAK_CHECKED, 0.5F)), ANCIENT_OAK_CHECKED));
+    public static final RegistryObject<PlacedFeature> CORRUPTED_TREE_CHECKED = ModPlacedFeatures.PLACED_FEATURES.register("corrupted_tree_checked",
+            () -> new PlacedFeature(CORRUPTED_TREE.getHolder().get(),
+                    List.of(PlacementUtils.filteredByBlockSurvival(Blocks.SCULK))));
 
+    public static final RegistryObject<ConfiguredFeature<?, ?>> CORRUPTED_TREE_SPAWN =
+            CONFIGURED_FEATURES.register("corrupted_tree_spawn", () -> new ConfiguredFeature<>(Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(
+                            CORRUPTED_TREE_CHECKED.getHolder().get(),
+                            0.005F)), CORRUPTED_TREE_CHECKED.getHolder().get())));
+
+
+    public static void register(IEventBus eventBus) {
+        CONFIGURED_FEATURES.register(eventBus);
+    }
 }
--- src/main/java/net/vakror/soulbound/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/vakror/soulbound/world/feature/ModPlacedFeatures.java
@@ -1,13 +1,27 @@
 package net.vakror.soulbound.world.feature;
 
 import net.minecraft.core.Holder;
-import net.minecraft.data.worldgen.features.MiscOverworldFeatures;
+import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.placement.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.vakror.soulbound.SoulboundMod;
 
 public class ModPlacedFeatures {
-    public static final Holder<PlacedFeature> ANCIENT_OAK_PLACED = PlacementUtils.register("ancient_oak_placed",
-            ModConfiguredFeatures.ANCIENT_OAK_SPAWN, VegetationPlacements.treePlacement(RarityFilter.onAverageOnceEvery(3)));
+    public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
+            DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, SoulboundMod.MOD_ID);
 
+    public static final RegistryObject<PlacedFeature> CORRUPTED_TREE = PLACED_FEATURES.register("corrupted_tree_placed",
+            () -> new PlacedFeature(ModConfiguredFeatures.CORRUPTED_TREE_SPAWN.getHolder().get(), VegetationPlacements.treePlacement(
+                    PlacementUtils.countExtra(1, 0.1f, 3))));
+
+    public static void register(IEventBus eventBus) {
+        PLACED_FEATURES.register(eventBus);
+    }
 }
