11182aaa43b99f0fcddb8a2b189d7dbb4eab7da2
Vakror Alor <89393811+vaylor27@users.noreply.github.com>
2023-02-26 17:43:44 -0500

    Add TungstenIngotItem.java

---
+++ src/main/java/net/vakror/soulbound/items/custom/TungstenIngotItem.java
@@ -0,0 +1,9 @@
+package net.vakror.soulbound.items.custom;
+
+import net.minecraft.world.item.Item;
+
+public class TungstenIngotItem extends Item {
+    public TungstenIngotItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
