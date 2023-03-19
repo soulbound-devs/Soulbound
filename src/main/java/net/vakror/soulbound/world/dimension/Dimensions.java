ed9370fe3d573490472b9d129e0adf1d2827281f
Vakror Alor <89393811+vaylor27@users.noreply.github.com>
2023-03-19 17:30:04 -0400

    Created dimension keys

---
+++ src/main/java/net/vakror/soulbound/world/dimension/Dimensions.java
@@ -0,0 +1,19 @@
+package net.vakror.soulbound.world.dimension;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.vakror.soulbound.SoulboundMod;
+
+
+public class Dimensions {
+    public static final ResourceKey<Level> DUNGEON_DIM_KEY = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(SoulboundMod.MOD_ID, "dungeon"));
+    public static final ResourceKey<DimensionType> DUNGEON_TYPE = ResourceKey.create(Registry.DIMENSION_TYPE_REGISTRY, DUNGEON_DIM_KEY.location());
+
+    public static void register() {
+        SoulboundMod.LOGGER.info("Dimensions are being registered for Soulbound");
+    }
+
+}
