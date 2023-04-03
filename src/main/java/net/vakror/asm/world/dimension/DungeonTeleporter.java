0027cf761a2d8da0f84f996b223cc8e6a1688736
Vakror Alor <89393811+vaylor27@users.noreply.github.com>
2023-04-03 09:29:01 -0400

    Update DungeonAccessBlock.java

--- src/main/java/net/vakror/asm/world/dimension/DungeonTeleporter.java
+++ src/main/java/net/vakror/asm/world/dimension/DungeonTeleporter.java
@@ -4,15 +4,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.level.portal.PortalInfo;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.util.ITeleporter;
 import net.vakror.asm.blocks.ModBlocks;
 import net.vakror.asm.blocks.custom.DungeonAccessBlock;
 import net.vakror.asm.blocks.entity.custom.DungeonAccessBlockEntity;
-import net.vakror.asm.world.structure.ModStructures;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
@@ -31,7 +28,6 @@ public class DungeonTeleporter implements ITeleporter {
     @Override
     public @Nullable PortalInfo getPortalInfo(Entity entity, ServerLevel destWorld, Function<ServerLevel, PortalInfo> defaultPortalInfo) {
         if (!(destWorld.getBlockEntity(pos) instanceof DungeonAccessBlockEntity)) {
-            StructureStart structureStart = ModStructures.DUNGEON.get().generate(destWorld.registryAccess(), destWorld.getChunkSource().getGenerator(), destWorld.getChunkSource().getGenerator().getBiomeSource(), destWorld.getChunkSource().randomState(), destWorld.getStructureManager(), destWorld.getSeed(), new ChunkPos(pos), 0, destWorld, (biomeHolder) -> {return true;});
             destWorld.setBlock(pos, ModBlocks.DUNGEON_KEY_BLOCK.get().defaultBlockState().setValue(DungeonAccessBlock.TO_OVERWORLD, true), 3);
         }
         return new PortalInfo(new Vec3(pos.above().getX(), pos.above().getY(), pos.above().getZ()), entity.getDeltaMovement(), entity.getYRot(), entity.getXRot());
