f77e544f96ed7d3318079e3edea242ec4616e541
vakror <vaylor2027@gmail.com>
2023-04-09 19:18:31 -0400

    Add data generators

---
+++ src/main/java/net/vakror/asm/datagen/ModWorldGenProvider.java
@@ -0,0 +1,25 @@
+package net.vakror.asm.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.vakror.asm.ASMMod;
+import net.vakror.asm.world.feature.ModConfiguredFeatures;
+import net.vakror.asm.world.structure.ModStructures;
+
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder().add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModConfiguredFeatures::placedBootstrap)
+            .add(Registries.STRUCTURE_SET, ModStructures::bootstrapSets)
+            .add(Registries.STRUCTURE, ModStructures::bootstrap);
+
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Collections.singleton(ASMMod.MOD_ID));
+    }
+
+}
