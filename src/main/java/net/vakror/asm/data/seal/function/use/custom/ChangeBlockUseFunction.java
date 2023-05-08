ea6f5c4ea23e73b0c5592718ec855e16d073d9f7
Vakror Alor <89393811+vaylor27@users.noreply.github.com>
2023-05-08 11:47:16 -0400

    Add classes for use functions

---
+++ src/main/java/net/vakror/asm/data/seal/function/use/custom/ChangeBlockUseFunction.java
@@ -0,0 +1,23 @@
+package net.vakror.asm.data.seal.function.use.custom;
+
+import com.google.gson.JsonObject;
+import net.minecraft.world.item.context.UseOnContext;
+import net.vakror.asm.data.seal.function.use.UseFunction;
+
+import java.nio.file.Path;
+
+public class ChangeBlockUseFunction extends UseFunction {
+    public ChangeBlockUseFunction(JsonObject functionObject, Path path) {
+        super(functionObject, path);
+    }
+
+    @Override
+    public void readFromJson(JsonObject function, Path path) {
+
+    }
+
+    @Override
+    public void executeUse(UseOnContext context) {
+
+    }
+}
