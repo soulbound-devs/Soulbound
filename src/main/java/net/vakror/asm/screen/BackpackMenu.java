4e385389ab272e90720040c2da4318dcc55afcf0
Vakror Alor <89393811+vaylor27@users.noreply.github.com>
2023-05-08 21:12:31 -0400

    Create BackpackMenu.java

---
+++ src/main/java/net/vakror/asm/screen/BackpackMenu.java
@@ -0,0 +1,152 @@
+package net.vakror.asm.screen;
+
+import com.google.common.collect.Lists;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.core.NonNullList;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.client.gui.widget.ScrollPanel;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+import net.vakror.asm.ASMMod;
+import net.vakror.asm.blocks.ModBlocks;
+import net.vakror.asm.blocks.entity.custom.WandImbuingTableBlockEntity;
+import net.vakror.asm.screen.slot.ModResultSlot;
+import net.vakror.asm.screen.slot.ModSealSlot;
+import net.vakror.asm.screen.slot.ModWandSlot;
+import org.cyclops.cyclopscore.client.gui.container.ContainerScreenScrolling;
+import org.cyclops.cyclopscore.inventory.LargeInventory;
+import org.cyclops.cyclopscore.inventory.container.ScrollingInventoryContainer;
+import org.cyclops.cyclopscore.inventory.slot.SlotExtended;
+
+import java.io.FileReader;
+import java.util.Collections;
+import java.util.List;
+
+public class BackpackMenu extends ScrollingInventoryContainer<Slot> {
+    public int ROWS = 3;
+    public int COLUMNS = 9;
+    private final List<Slot> slots;
+    private final NonNullList<ItemStack> stacks;
+
+    public BackpackMenu(int pContainerId, Inventory inv, FriendlyByteBuf data) {
+        this(pContainerId, inv, new SimpleContainerData(1), data.readItem(), new LargeInventory(data.readInt(), 64);
+    }
+
+    public BackpackMenu(int id, Inventory playerInv, ContainerData data, ItemStack wand, Container inventory) {
+        super(ModMenuTypes.BACKPACK_MENU.get(), id, playerInv, inventory, Collections.<Slot>emptyList(), (item, pattern) -> true);
+        checkContainerSize(playerInv, 27);
+
+        addPlayerInventory(playerInv);
+        addPlayerHotbar(playerInv);
+
+        this.slots = Lists.newArrayListWithCapacity(getSizeInventory());
+        this.stacks = NonNullList.withSize(getSizeInventory(), ItemStack.EMPTY);
+
+        this.addSlots(getSizeInventory() / COLUMNS, COLUMNS);
+    }
+
+    private void addSlots(int rows, int columns) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                Slot slot = createSlot(inventory, column + row * columns, 9 + column * 18, 18 + row * 18);
+                addSlot(slot);
+                slots.add(slot);
+            }
+        }
+    }
+
+    @Override
+    protected List<Slot> getUnfilteredItems() {
+        return this.slots;
+    }
+
+    @Override
+    public int getColumns() {
+        return COLUMNS;
+    }
+
+    @Override
+    public int getPageSize() {
+        return ROWS;
+    }
+
+    private Slot createSlot(Container inventory, int index, int row, int column) {
+        return new SlotExtended(inventory, index, row, column);
+    }
+
+
+    // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
+    // must assign a slot number to each of the slots used by the GUI.
+    // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
+    // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
+    //  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
+    //  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
+    //  36 - 44 = TileInventory slots, which map to our TileEntity slot numbers 0 - 8)
+    private static final int HOTBAR_SLOT_COUNT = 9;
+    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
+    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
+    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
+    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
+    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
+
+    // THIS YOU HAVE TO DEFINE FOR CUSTOM BLOCK ENTITIES!!!!!
+    private static final int TE_INVENTORY_SLOT_COUNT = 4;  // must be the number of slots you have!
+
+    @Override
+    public ItemStack quickMoveStack(Player playerIn, int index) {
+        Slot sourceSlot = slots.get(index);
+        if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;  //EMPTY_ITEM
+        ItemStack sourceStack = sourceSlot.getItem();
+        ItemStack copyOfSourceStack = sourceStack.copy();
+
+        // Check if the slot clicked is one of the vanilla container slots
+        if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
+            // This is a vanilla container slot so merge the stack into the tile inventory
+            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
+                    + TE_INVENTORY_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;  // EMPTY_ITEM
+            }
+        } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
+            // This is a TE slot so merge the stack into the players inventory
+            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+                return ItemStack.EMPTY;
+            }
+        } else {
+            ASMMod.LOGGER.info("Invalid slotIndex:" + index);
+            return ItemStack.EMPTY;
+        }
+        // If stack size == 0 (the entire stack was moved) set slot contents to null
+        if (sourceStack.getCount() == 0) {
+            sourceSlot.set(ItemStack.EMPTY);
+        } else {
+            sourceSlot.setChanged();
+        }
+        sourceSlot.onTake(playerIn, sourceStack);
+        return copyOfSourceStack;
+    }
+    private void addPlayerInventory(Inventory playerInventory) {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; l < 9; ++l) {
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for (int i = 0; i < 9; ++i) {
+            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
+        }
+    }
+}
