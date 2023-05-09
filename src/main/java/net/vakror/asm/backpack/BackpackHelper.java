1c3725c40e4df2e3c58f83d5dffb6e92a1be44cf
Vakror Alor <89393811+vaylor27@users.noreply.github.com>
2023-05-08 21:12:17 -0400

    Create BackpackHelper.java

---
+++ src/main/java/net/vakror/asm/backpack/BackpackHelper.java
@@ -0,0 +1,15 @@
+package net.vakror.asm.backpack;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.network.NetworkHooks;
+
+public class BackpackHelper {
+    public static void openBackpackGui(Player player, ItemStack stack) {
+        if (player instanceof ServerPlayer) {
+            NetworkHooks.openScreen(((ServerPlayer) player), );
+        }
+    }
+}
