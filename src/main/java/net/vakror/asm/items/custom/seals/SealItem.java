691239fb8fbd8d77c8c0101cef358b58e6e613bf
vakror <vaylor2027@gmail.com>
2023-06-13 20:03:35 -0400

    Allow for seal stack configuration

--- src/main/java/net/vakror/asm/items/custom/seals/SealItem.java
+++ src/main/java/net/vakror/asm/items/custom/seals/SealItem.java
@@ -7,6 +7,7 @@ public class SealItem extends Item {
     private final String id;
     private final SealType type;
     private final boolean canAddMultiple;
+    private final int maxSealStack;
 
     public SealType getType() {
         return type;
@@ -17,17 +18,22 @@ public class SealItem extends Item {
     }
 
     public SealItem(Properties pProperties, String id, SealType type) {
-        this(pProperties, id, type, false);
+        this(pProperties, id, type, false, 1);
     }
 
-    public SealItem(Properties pProperties, String id, SealType type, boolean canAddMultiple) {
+    public SealItem(Properties pProperties, String id, SealType type, boolean canAddMultiple, int maxSealStack) {
         super(pProperties);
         this.id = id;
         this.type = type;
         this.canAddMultiple = canAddMultiple;
+        this.maxSealStack = maxSealStack;
     }
 
     public boolean canAddMultiple() {
         return canAddMultiple;
+    }
+
+    public int getMaxSealStack() {
+        return maxSealStack;
     }
 }
