16b645cdfab50bafb3a5e0e7f26f4112731ea7d3
vakror <vaylor2027@gmail.com>
2023-06-17 16:37:26 -0400

    make BaseSeal use BetterArrayList

--- src/main/java/net/vakror/asm/seal/type/BaseSeal.java
+++ src/main/java/net/vakror/asm/seal/type/BaseSeal.java
@@ -2,13 +2,13 @@ package net.vakror.asm.seal.type;
 
 import net.vakror.asm.seal.ISeal;
 import net.vakror.asm.seal.SealProperty;
+import net.vakror.asm.util.BetterArrayList;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class BaseSeal implements ISeal {
     private final String id;
-    protected final List<SealProperty> properties = new ArrayList<>();
+    protected final List<SealProperty> properties = new BetterArrayList<>();
     private final boolean canBeActivated;
 
     public BaseSeal(String id, boolean canBeActivated) {
