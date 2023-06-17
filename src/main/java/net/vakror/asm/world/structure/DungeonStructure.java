89492d776e312ac3727e36e55869d7222a648ba4
vakror <vaylor2027@gmail.com>
2023-06-17 16:35:54 -0400

    remove useless method

--- src/main/java/net/vakror/asm/world/structure/DungeonStructure.java
+++ src/main/java/net/vakror/asm/world/structure/DungeonStructure.java
@@ -2,28 +2,19 @@ package net.vakror.asm.world.structure;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.LevelHeightAccessor;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.WorldGenerationContext;
 import net.minecraft.world.level.levelgen.heightproviders.UniformHeight;
 import net.minecraft.world.level.levelgen.structure.Structure;
-import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
-import java.util.function.Predicate;
 
 public class DungeonStructure extends Structure {
     public static final Codec<DungeonStructure> CODEC = simpleCodec(DungeonStructure::new);
@@ -57,10 +48,7 @@ public class DungeonStructure extends Structure {
         DungeonPiece.generateDungeon(pos, rot, manager, builder, this.size, this.layer);
     }
 
-    public @NotNull StructureStart generate(RegistryAccess pRegistryAccess, ChunkGenerator pChunkGenerator, BiomeSource pBiomeSource, RandomState pRandomState, StructureTemplateManager pStructureTemplateManager, long pSeed, ChunkPos pChunkPos, int p_226604_, LevelHeightAccessor pHeightAccessor, Predicate<Holder<Biome>> pValidBiome) {
-        return super.generate(pRegistryAccess, pChunkGenerator, pBiomeSource, pRandomState, pStructureTemplateManager, pSeed, pChunkPos, p_226604_, pHeightAccessor, pValidBiome);
-    }
-
+    @Override
     public StructureType<?> type() {
         return ModStructures.DUNGEON_TYPE.get();
     }
