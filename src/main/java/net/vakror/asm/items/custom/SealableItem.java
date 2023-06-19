6ce51793f04e28bc2c2908347cdc6b3d8f5111d5
vakror <vaylor2027@gmail.com>
2023-06-18 23:55:02 -0400

    add whitespaces

--- src/main/java/net/vakror/asm/items/custom/SealableItem.java
+++ src/main/java/net/vakror/asm/items/custom/SealableItem.java
@@ -370,7 +370,7 @@ public class SealableItem extends DiggerItem {
             } else if (!found && Character.isLetter(chars[i])) {
                 chars[i] = Character.toUpperCase(chars[i]);
                 found = true;
-            } else if (Character.isWhitespace(chars[i]) || chars[i]=='.') { // You can add other chars here
+            } else if (Character.isWhitespace(chars[i]) || chars[i] == '.') { // You can add other chars here
                 found = false;
             }
         }
