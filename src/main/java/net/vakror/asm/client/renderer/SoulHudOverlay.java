68cd51dad0519ce0e9e4537ee192d4b5267e154b
vakror <vaylor2027@gmail.com>
2023-06-24 17:47:54 -0400

    add text to soul hud

--- src/main/java/net/vakror/asm/client/renderer/SoulHudOverlay.java
+++ src/main/java/net/vakror/asm/client/renderer/SoulHudOverlay.java
@@ -1,6 +1,7 @@
 package net.vakror.asm.client.renderer;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
@@ -32,19 +33,24 @@ public class SoulHudOverlay {
         float maxDarkSoul = (int) ClientSoulData.getDarkPlayerMaxSoul();
 
         int darkSoulHeight = (int) (darkSoul/maxDarkSoul * 56);
-        int adjustedDarkSoulHeight = (int)Math.ceil(darkSoul/maxDarkSoul * 56);
+        int adjustedDarkSoulHeight = (int)Math.floor(darkSoul/maxDarkSoul * 56);
         adjustedDarkSoulHeight = adjustedDarkSoulHeight % 2 == 0 ? adjustedDarkSoulHeight: adjustedDarkSoulHeight + 1;
 
         int soulHeight = (int) (soul/maxSoul * 56);
-        int adjustedSoulHeight = (int)Math.ceil(soul/maxSoul * 56);
+        int adjustedSoulHeight = (int)Math.floor(soul/maxSoul * 56);
         graphics.blit(SOUL_BAR, x - 175, y - 70 + (56 - adjustedSoulHeight),0, 0, 28, soulHeight,
                 28, soulHeight);
 
-        graphics.blit(DARK_SOUL_BAR, x - 125, y - 70 + (56 - adjustedDarkSoulHeight),0, 0, 28, darkSoulHeight,
+        int soulWidth = Minecraft.getInstance().font.width((int) soul + "/" + (int) maxSoul);
+        int darkSoulWidth = Minecraft.getInstance().font.width((int) darkSoul + "/" + (int) maxDarkSoul);
+        graphics.drawString(Minecraft.getInstance().font, (int) soul + "/" +(int)  maxSoul,x - 145 - soulWidth, y - 80, toColorInt(255, 255, 255, 255), false);
+        graphics.drawString(Minecraft.getInstance().font, (int) darkSoul + "/" + (int) maxDarkSoul,x - 98 - darkSoulWidth, y - 80, toColorInt(255, 255, 255, 255), false);
+
+        graphics.blit(DARK_SOUL_BAR, x - 130, y - 70 + (56 - adjustedDarkSoulHeight),0, 0, 28, darkSoulHeight,
                 28, darkSoulHeight);
 
         graphics.blit(SOUL_OVERLAY, x - 175, y - 70, 0, 0, 28, 56, 28, 56);
-        graphics.blit(DARK_SOUL_OVERLAY, x - 125, y - 70, 0, 0, 28, 56, 28, 56);
+        graphics.blit(DARK_SOUL_OVERLAY, x - 130, y - 70, 0, 0, 28, 56, 28, 56);
     });
 
     public static int toColorInt(int R, int G, int B, int A) {
