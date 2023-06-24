1c66fb650b0a4d7f5a2d5c16077ebf6b4472ed59
vakror <vaylor2027@gmail.com>
2023-06-23 23:57:14 -0400

    fix Axing seal not being offensive; change some class hierarchy

--- src/main/java/net/vakror/asm/seal/seals/activatable/tool/OffensiveToolSeal.java
+++ src/main/java/net/vakror/asm/seal/seals/activatable/tool/OffensiveToolSeal.java
@@ -3,18 +3,18 @@ package net.vakror.asm.seal.seals.activatable.tool;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.vakror.asm.seal.SealProperty;
-import net.vakror.asm.seal.type.AttackSeal;
 
 import java.util.List;
 
-public abstract class OffensiveToolSeal extends AttackSeal {
-    public final TagKey<Block> mineableBlocks;
-
-
+public abstract class OffensiveToolSeal extends ToolSeal {
 
     public OffensiveToolSeal(String id, TagKey<Block> mineableBlocks, float swingSpeed) {
-        super(id, swingSpeed);
-        this.mineableBlocks = mineableBlocks;
+        super(id, mineableBlocks, swingSpeed);
+    }
+
+    @Override
+    public boolean isAttack() {
+        return true;
     }
 
     @Override
