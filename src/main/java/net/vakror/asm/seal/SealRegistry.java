58cc1f7630da9af3708e442c6d01d9c50868443b
vakror <vaylor2027@gmail.com>
2023-06-23 23:58:53 -0400

    add sealItemGetter

--- src/main/java/net/vakror/asm/seal/SealRegistry.java
+++ src/main/java/net/vakror/asm/seal/SealRegistry.java
@@ -1,6 +1,9 @@
 package net.vakror.asm.seal;
 
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
+import net.vakror.asm.items.ModItems;
 import net.vakror.asm.seal.seals.activatable.SwordSeal;
 import net.vakror.asm.seal.seals.activatable.tool.AxingSeal;
 import net.vakror.asm.seal.seals.activatable.tool.HoeingSeal;
@@ -20,19 +23,20 @@ public class SealRegistry {
     public static Map<String, ISeal> passiveSeals = new HashMap<>();
     public static Map<String, ISeal> attackSeals = new HashMap<>();
     public static Map<String, ISeal> amplifyingSeals = new HashMap<>();
+    public static Map<String, RegistryObject<Item>> sealItemGetter = new HashMap<>();
 
     public static void registerSeals() {
-        addAttackSeal(new AxingSeal());
-        addPassiveSeal(new PickaxingSeal());
-        addPassiveSeal(new HoeingSeal());
-        addAmplifyingSealSeal(new HasteSeal.HasteSealTierOne());
-        addAmplifyingSealSeal(new HasteSeal.HasteSealTierTwo());
-        addAmplifyingSealSeal(new HasteSeal.HasteSealTierThree());
-        addAmplifyingSealSeal(new PickupSeal());
-        addAmplifyingSealSeal(new StackSizeUpgradeSeal(1, 2, ArithmeticActionType.MULTIPLY));
-        addAmplifyingSealSeal(new ColumnUpgradeSeal(1, 2, ArithmeticActionType.ADD));
-        addAmplifyingSealSeal(new RowUpgradeSeal(1, 2, ArithmeticActionType.ADD));
-        addAttackSeal(new SwordSeal());
+        addAttackSeal(new AxingSeal(), ModItems.AXING_SEAL);
+        addPassiveSeal(new PickaxingSeal(), ModItems.PICKAXING_SEAL);
+        addPassiveSeal(new HoeingSeal(), ModItems.HOEING_SEAL);
+        addAmplifyingSealSeal(new HasteSeal.HasteSealTierOne(), ModItems.MINING_SPEED_SEAL_TIER_1);
+        addAmplifyingSealSeal(new HasteSeal.HasteSealTierTwo(), ModItems.MINING_SPEED_SEAL_TIER_2);
+        addAmplifyingSealSeal(new HasteSeal.HasteSealTierThree(), ModItems.MINING_SPEED_SEAL_TIER_3);
+        addAmplifyingSealSeal(new PickupSeal(), ModItems.SACK_PICKUP_SEAL);
+        addAmplifyingSealSeal(new StackSizeUpgradeSeal(1, 2, ArithmeticActionType.MULTIPLY), ModItems.SACK_STACK_SIZE_UPGRADE_SEAL_TIER_1);
+        addAmplifyingSealSeal(new ColumnUpgradeSeal(1, 2, ArithmeticActionType.ADD), ModItems.SACK_COLUMN_UPGRADE_SEAL_TIER_1);
+        addAmplifyingSealSeal(new RowUpgradeSeal(1, 2, ArithmeticActionType.ADD), ModItems.SACK_ROW_UPGRADE_SEAL_TIER_1);
+        addAttackSeal(new SwordSeal(), ModItems.SWORDING_SEAL);
     }
 
     public static void addSeal(ISeal seal, SealType type) {
@@ -44,19 +48,21 @@ public class SealRegistry {
         allSeals.put(seal.getId(), seal);
     }
 
-    public static void addAttackSeal(ISeal seal) {
+    public static void addAttackSeal(ISeal seal, RegistryObject<Item> sealItem) {
         attackSeals.put(seal.getId(), seal);
         allSeals.put(seal.getId(), seal);
-
+        sealItemGetter.put(seal.getId(), sealItem);
     }
 
-    public static void addPassiveSeal(ISeal seal) {
+    public static void addPassiveSeal(ISeal seal, RegistryObject<Item> sealItem) {
         passiveSeals.put(seal.getId(), seal);
         allSeals.put(seal.getId(), seal);
+        sealItemGetter.put(seal.getId(), sealItem);
     }
 
-    public static void addAmplifyingSealSeal(ISeal seal) {
+    public static void addAmplifyingSealSeal(ISeal seal, RegistryObject<Item> sealItem) {
         amplifyingSeals.put(seal.getId(), seal);
         allSeals.put(seal.getId(), seal);
+        sealItemGetter.put(seal.getId(), sealItem);
     }
 }
