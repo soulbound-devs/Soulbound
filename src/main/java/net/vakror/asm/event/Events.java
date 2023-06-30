653d94cf3549e9e59e17aabd5d5f6f05ebfe151e
vakror <vaylor2027@gmail.com>
2023-06-30 18:24:08 -0400

    remove code that was crashing the game

--- src/main/java/net/vakror/asm/event/Events.java
+++ src/main/java/net/vakror/asm/event/Events.java
@@ -89,9 +89,9 @@ public class Events {
 
         @SubscribeEvent
         public static void triggerScytheAOE(LivingHurtEvent event) {
-            if (event.getEntity() instanceof GoblaggerEntity && (isDamageType(event.getSource(), DamageTypes.CRAMMING) || isDamageType(event.getSource(), DamageTypes.IN_WALL) || isDamageType(event.getSource(), DamageTypes.FALL))) {
-                event.setCanceled(true);
-            }
+//            if (event.getEntity() instanceof GoblaggerEntity && (isDamageType(event.getSource(), DamageTypes.CRAMMING) || isDamageType(event.getSource(), DamageTypes.IN_WALL) || isDamageType(event.getSource(), DamageTypes.FALL))) {
+//                event.setCanceled(true);
+//            }
             if (event.getSource().getEntity() instanceof ServerPlayer player) {
                 if (player.getMainHandItem().getItem() instanceof SealableItem || player.getOffhandItem().getItem() instanceof SealableItem && event.getSource().getEntity() instanceof Player) {
                     InteractionHand hand;
