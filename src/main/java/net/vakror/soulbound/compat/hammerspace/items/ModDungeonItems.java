ed1e8d7476d5363ce9d03fba1a7a08b89d7404d0
vakror <vaylor2027@gmail.com>
2023-08-04 17:08:35 -0400

    add key to creative mode tab

--- src/main/java/net/vakror/soulbound/compat/hammerspace/items/ModDungeonItems.java
+++ src/main/java/net/vakror/soulbound/compat/hammerspace/items/ModDungeonItems.java
@@ -6,13 +6,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.vakror.soulbound.SoulboundMod;
+import net.vakror.soulbound.tab.ModCreativeModeTabs;
 
 public class ModDungeonItems {
     public static final DeferredRegister<Item> ITEMS_REGISTRY =
             DeferredRegister.create(ForgeRegistries.ITEMS, SoulboundMod.MOD_ID);
 
     public static final RegistryObject<Item> KEY = ITEMS_REGISTRY.register("key",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.SOULBOUND_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS_REGISTRY.register(eventBus);
