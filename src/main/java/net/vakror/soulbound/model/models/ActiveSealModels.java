e5395ed026a6e0321e6fcd1d940740523d302279
vaylor27 <godisagodlol@gmail.com>
2023-08-19 21:36:39 -0400

    add swording models to active seal models

--- src/main/java/net/vakror/soulbound/model/models/ActiveSealModels.java
+++ src/main/java/net/vakror/soulbound/model/models/ActiveSealModels.java
@@ -14,7 +14,7 @@ public class ActiveSealModels {
         builder.put("pickaxing", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/pickaxing/base.obj"));
         builder.put("hoeing", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/hoeing/base.obj"));
         builder.put("shoveling", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/pickaxing/base.obj"));
-        builder.put("swording", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/pickaxing/base.obj"));
+        builder.put("swording", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/swording/base.obj"));
         builder.put("axing", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/axing/base.obj"));
         builder.put("scythe", new ResourceLocation(SoulboundMod.MOD_ID, "models/obj/active_seal/pickaxing/base.obj"));
         MODELS = builder.build();
