4cb51f891721b62b69ab622eff81cf35eb822f69
vaylor27 <godisagodlol@gmail.com>
2023-08-29 11:02:29 -0400

    remove TungstenIngotItem.java

--- src/main/java/net/vakror/soulbound/mod/items/custom/TungstenIngotItem.java
+++
@@ -1,9 +0,0 @@
-package net.vakror.soulbound.mod.items.custom;
-
-import net.minecraft.world.item.Item;
-
-public class TungstenIngotItem extends Item {
-    public TungstenIngotItem(Properties pProperties) {
-        super(pProperties);
-    }
-}
