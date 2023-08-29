0be35e10f9189a25fb2be16fc1cce557e102c35c
vaylor27 <godisagodlol@gmail.com>
2023-08-29 11:05:18 -0400

    remove ColorUtil.java

--- src/main/java/net/vakror/soulbound/mod/util/ColorUtil.java
+++
@@ -1,8 +0,0 @@
-package net.vakror.soulbound.mod.util;
-
-public class ColorUtil {
-
-    public static int toColorInt(int R, int G, int B, int A) {
-        return (A & 0xff) << 24 | (R & 0xff) << 16 | (G & 0xff) << 8 | (B & 0xff);
-    }
-}
