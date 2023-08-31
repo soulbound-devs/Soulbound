8a9417f26e7c9f2fba2482b86724f077e9d5b99d
vaylor27 <godisagodlol@gmail.com>
2023-08-31 17:44:39 -0400

    add javadocs

--- src/main/java/net/vakror/soulbound/api/context/IRegistrationContext.java
+++ src/main/java/net/vakror/soulbound/api/context/IRegistrationContext.java
@@ -1,5 +1,9 @@
 package net.vakror.soulbound.api.context;
 
+/**
+ * Simple class for contexts to extend to get their name.
+ * Name is used in extensions to know which context to use to register
+ */
 public interface IRegistrationContext {
     /**
      * @return the name of this context (soulbound uses "default").
